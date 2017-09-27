2c118d02ceccdf7191c91c249145a83c34581b37
kev7862 <kevmatite7862@gmail.com>
2017-09-27 14:20:28 +0300

    Declared my global variables

--- app/src/main/java/kev7862/github/weatherforcast/DownloadTask.java
+++ app/src/main/java/kev7862/github/weatherforcast/DownloadTask.java
@@ -1,7 +1,22 @@
 package kev7862.github.weatherforcast;
+
+import android.os.AsyncTask;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by matite on 9/27/17.
  */
+//This is where we download the webpage data.
+public class DownloadTask extends AsyncTask<String, Void, String>{
 
-public class DownloadTask {
+    String results = "";
+    URL url;
+    HttpURLConnection urlConnection = null;
+
+    @Override
+    protected String doInBackground(String... urls) {
+        return null;
+    }
 }
